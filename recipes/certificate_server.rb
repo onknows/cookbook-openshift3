ea754225d6a7b8a7894fbace30faa666b55721ea
William Burton <william17.burton@gmail.com>
2017-08-03 10:14:07 +0100

    - Refactor the certificate piece

--- recipes/certificate_server.rb
+++ recipes/certificate_server.rb
@@ -1,20 +1,45 @@
-master_servers = node['cookbook-openshift3']['master_servers']
-certificate_server = node['cookbook-openshift3']['certificate_server'] == {} ? master_servers.first : node['cookbook-openshift3']['certificate_server']
+certificate_server = node['cookbook-openshift3']['certificate_server'] == {} ? node['cookbook-openshift3']['master_servers'] : node['cookbook-openshift3']['master_servers'] << node['cookbook-openshift3']['certificate_server']
 
-# Certificate server needs oadm installed
-package node['cookbook-openshift3']['openshift_service_type'] do
-  action :install
-  version node['cookbook-openshift3']['ose_version'] unless node['cookbook-openshift3']['ose_version'].nil?
-  only_if { certificate_server['fqdn'] == node['fqdn'] }
-end
+if certificate_server.find { |server_master| server_master['fqdn'] == node['fqdn'] }
+  if node['cookbook-openshift3']['deploy_containerized']
+    execute 'Pull CLI docker image' do
+      command "docker pull #{node['cookbook-openshift3']['openshift_docker_cli_image']}:#{node['cookbook-openshift3']['openshift_docker_image_version']}"
+      not_if "docker images  | grep #{node['cookbook-openshift3']['openshift_docker_cli_image']}.*#{node['cookbook-openshift3']['openshift_docker_image_version']}"
+    end
 
-# If this is the certificate server, create the master certs. TODO: refactor this.
-execute 'Create the master certificates' do
-  command "#{node['cookbook-openshift3']['openshift_common_admin_binary']} ca create-master-certs \
-          --hostnames=#{(node['cookbook-openshift3']['erb_corsAllowedOrigins'] + [node['cookbook-openshift3']['openshift_common_ip']]).uniq.join(',')} \
-          --master=#{node['cookbook-openshift3']['openshift_master_api_url']} \
-          --public-master=#{node['cookbook-openshift3']['openshift_master_public_api_url']} \
-          --cert-dir=#{node['cookbook-openshift3']['openshift_master_config_dir']} --overwrite=false"
-  creates "#{node['cookbook-openshift3']['openshift_master_config_dir']}/master.server.key"
-  only_if { certificate_server['fqdn'] == node['fqdn'] }
+    bash 'Add CLI to master(s)' do
+      code <<-EOH
+        docker create --name temp-cli ${DOCKER_IMAGE}:${DOCKER_TAG}
+        docker cp temp-cli:/usr/bin/openshift /usr/local/bin/openshift
+        docker rm temp-cli
+        EOH
+      environment(
+        'DOCKER_IMAGE' => node['cookbook-openshift3']['openshift_docker_master_image'],
+        'DOCKER_TAG' => node['cookbook-openshift3']['openshift_docker_image_version']
+      )
+      not_if { ::File.exist?('/usr/local/bin/openshift') }
+    end
+
+    %w(oadm oc kubectl).each do |client_symlink|
+      link "/usr/local/bin/#{client_symlink}" do
+        to '/usr/local/bin/openshift'
+        link_type :hard
+      end
+    end
+
+    execute 'Add bash completion for oc' do
+      command '/usr/local/bin/oc completion bash > /etc/bash_completion.d/oc'
+      not_if { ::File.exist?('/etc/bash_completion.d/oc') || node['cookbook-openshift3']['openshift_docker_image_version'] =~ /v1.2/i }
+    end
+
+    execute 'Pull MASTER docker image' do
+      command "docker pull #{node['cookbook-openshift3']['openshift_docker_master_image']}:#{node['cookbook-openshift3']['openshift_docker_image_version']}"
+      not_if "docker images  | grep #{node['cookbook-openshift3']['openshift_docker_master_image']}.*#{node['cookbook-openshift3']['openshift_docker_image_version']}"
+    end
+  end
+
+  package node['cookbook-openshift3']['openshift_service_type'] do
+    version node['cookbook-openshift3'] ['ose_version'] unless node['cookbook-openshift3']['ose_version'].nil?
+    not_if { node['cookbook-openshift3']['deploy_containerized'] }
+  end
 end
--- recipes/master.rb
+++ recipes/master.rb
@@ -10,50 +10,6 @@ certificate_server = node['cookbook-openshift3']['certificate_server'] == {} ? m
 
 include_recipe 'cookbook-openshift3::etcd_cluster'
 
-if master_servers.find { |server_master| server_master['fqdn'] == node['fqdn'] }
-  if node['cookbook-openshift3']['deploy_containerized']
-    execute 'Pull CLI docker image' do
-      command "docker pull #{node['cookbook-openshift3']['openshift_docker_cli_image']}:#{node['cookbook-openshift3']['openshift_docker_image_version']}"
-      not_if "docker images  | grep #{node['cookbook-openshift3']['openshift_docker_cli_image']}.*#{node['cookbook-openshift3']['openshift_docker_image_version']}"
-    end
-
-    bash 'Add CLI to master(s)' do
-      code <<-EOH
-        docker create --name temp-cli ${DOCKER_IMAGE}:${DOCKER_TAG}
-        docker cp temp-cli:/usr/bin/openshift /usr/local/bin/openshift
-        docker rm temp-cli
-        EOH
-      environment(
-        'DOCKER_IMAGE' => node['cookbook-openshift3']['openshift_docker_master_image'],
-        'DOCKER_TAG' => node['cookbook-openshift3']['openshift_docker_image_version']
-      )
-      not_if { ::File.exist?('/usr/local/bin/openshift') }
-    end
-
-    %w(oadm oc kubectl).each do |client_symlink|
-      link "/usr/local/bin/#{client_symlink}" do
-        to '/usr/local/bin/openshift'
-        link_type :hard
-      end
-    end
-
-    execute 'Add bash completion for oc' do
-      command '/usr/local/bin/oc completion bash > /etc/bash_completion.d/oc'
-      not_if { ::File.exist?('/etc/bash_completion.d/oc') || node['cookbook-openshift3']['openshift_docker_image_version'] =~ /v1.2/i }
-    end
-
-    execute 'Pull MASTER docker image' do
-      command "docker pull #{node['cookbook-openshift3']['openshift_docker_master_image']}:#{node['cookbook-openshift3']['openshift_docker_image_version']}"
-      not_if "docker images  | grep #{node['cookbook-openshift3']['openshift_docker_master_image']}.*#{node['cookbook-openshift3']['openshift_docker_image_version']}"
-    end
-  end
-
-  package node['cookbook-openshift3']['openshift_service_type'] do
-    version node['cookbook-openshift3'] ['ose_version'] unless node['cookbook-openshift3']['ose_version'].nil?
-    not_if { node['cookbook-openshift3']['deploy_containerized'] }
-  end
-end
-
 if certificate_server['fqdn'] == node['fqdn']
   package 'httpd' do
     notifies :run, 'ruby_block[Change HTTPD port xfer]', :immediately
