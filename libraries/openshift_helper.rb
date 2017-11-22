79d31bb59906e35b534b1b22dd4105d4dbc302c1
Ian Miell <ian.miell@gmail.com>
2017-11-22 17:24:41 +0000

    search

--- libraries/openshift_helper.rb
+++ libraries/openshift_helper.rb
@@ -6,7 +6,7 @@ module OpenShiftHelper
 
     def master_servers
       if !node['cookbook-openshift3']['openshift_cluster_duty_discovery_id'].nil? && node.run_list.roles.include?("#{node['cookbook-openshift3']['openshift_cluster_duty_discovery_id']}_use_role_based_duty_discovery")
-        search(:node, "role:#{node['cookbook-openshift3']['openshift_cluster_duty_discovery_id']}_openshift_master_duty")
+        Chef::Search::Query.new.search(:node, "role:#{node['cookbook-openshift3']['openshift_cluster_duty_discovery_id']}_openshift_master_duty")
       else
         node['cookbook-openshift3']['master_servers']
       end
