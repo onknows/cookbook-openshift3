7ee2bf67b085e69d74783419b87dd4b14ce02728
Ian Miell <ian.miell@gmail.com>
2017-12-15 08:36:04 +0000

    BUGFIX: revert to known working state

--- providers/openshift_create_pv.rb
+++ providers/openshift_create_pv.rb
@@ -15,7 +15,7 @@ action :create do
   new_resource.persistent_storage.each do |pv|
     execute "Create Persistent Storage : #{pv['name']}" do
       cwd node['cookbook-openshift3']['openshift_master_config_dir']
-      command "eval echo \'\{\"apiVersion\":\"v1\",\"kind\":\"PersistentVolume\",\"metadata\":\{\"name\":\"${name_pv}\"\},\"spec\":\{\"capacity\":\{\"storage\":\"${capacity_pv}\"\},\"accessModes\":[\"${access_modes_pv}\"],\"nfs\":\{\"path\":\"${path_pv}\",\"server\":\"${server_pv}\"\},\"persistentVolumeReclaimPolicy\":\"${volume_policy}\"\}\}\' | oc create -f - --config=#{node['cookbook-openshift3']['openshift_master_config_dir']}/admin.kubeconfig"
+      command 'eval echo \'\{\"apiVersion\":\"v1\",\"kind\":\"PersistentVolume\",\"metadata\":\{\"name\":\"${name_pv}\"\},\"spec\":\{\"capacity\":\{\"storage\":\"${capacity_pv}\"\},\"accessModes\":[\"${access_modes_pv}\"],\"nfs\":\{\"path\":\"${path_pv}\",\"server\":\"${server_pv}\"\},\"persistentVolumeReclaimPolicy\":\"${volume_policy}\"\}\}\' | oc create -f - --config=admin.kubeconfig'
       environment(
         'name_pv' => "#{pv['name']}-volume",
         'capacity_pv' => pv['capacity'],
@@ -24,20 +24,20 @@ action :create do
         'server_pv' => pv['server'],
         'volume_policy' => pv.key?('policy') ? pv['policy'] : 'Retain'
       )
-      not_if "[[ `oc get pv/${name_pv} --no-headers --config=#{node['cookbook-openshift3']['openshift_master_config_dir']}/admin.kubeconfig | wc -l` -eq 1 ]]"
+      not_if '[[ `oc get pv/${name_pv} --no-headers --config=admin.kubeconfig | wc -l` -eq 1 ]]'
     end
 
     next unless pv.key?('claim')
     execute "Create Persistent Claim: #{pv['name']}" do
       cwd node['cookbook-openshift3']['openshift_master_config_dir']
-      command "eval echo \'\{\"apiVersion\":\"v1\",\"kind\":\"PersistentVolumeClaim\",\"metadata\":\{\"name\":\"${name_pvc}\"\},\"spec\":\{\"resources\":\{\"requests\":\{\"storage\":\"${capacity_pvc}\"\}\},\"accessModes\":[\"${access_modes_pvc}\"]\}\}\' | oc create -f - --config=#{node['cookbook-openshift3']['openshift_master_config_dir']}/admin.kubeconfig -n ${namespace}"
+      command 'eval echo \'\{\"apiVersion\":\"v1\",\"kind\":\"PersistentVolumeClaim\",\"metadata\":\{\"name\":\"${name_pvc}\"\},\"spec\":\{\"resources\":\{\"requests\":\{\"storage\":\"${capacity_pvc}\"\}\},\"accessModes\":[\"${access_modes_pvc}\"]\}\}\' | oc create -f - --config=admin.kubeconfig -n ${namespace}'
       environment(
         'name_pvc' => "#{pv['name']}-claim",
         'capacity_pvc' => pv['capacity'],
         'access_modes_pvc' => pv['access_modes'],
         'namespace' => pv['claim']['namespace']
       )
-      not_if "[[ `oc get pvc/${name_pvc} --no-headers --config=#{node['cookbook-openshift3']['openshift_master_config_dir']}/admin.kubeconfig -n ${namespace} | wc -l` -eq 1 ]]"
+      not_if '[[ `oc get pvc/${name_pvc} --no-headers --config=admin.kubeconfig -n ${namespace} | wc -l` -eq 1 ]]'
     end
   end
   new_resource.updated_by_last_action(true)
