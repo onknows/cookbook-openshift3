6f3112a0f541aeab04970158ad8f7f8fcd3325d6
William Burton <william17.burton@gmail.com>
2018-02-26 12:53:52 +0000

    - Support openshift_node_port_range for configuring service NodePorts

--- attributes/default.rb
+++ attributes/default.rb
@@ -170,6 +170,7 @@ default['cookbook-openshift3']['openshift_node_generated_configs_dir'] = '/var/w
 default['cookbook-openshift3']['openshift_node_kubelet_args_default'] = { 'max-pods' => '250', 'image-gc-high-threshold' => '90', 'image-gc-low-threshold' => '80' }
 default['cookbook-openshift3']['openshift_node_kubelet_args_custom'] = {}
 default['cookbook-openshift3']['openshift_node_iptables_sync_period'] = '30s'
+default['cookbook-openshift3']['openshift_node_port_range'] = ''
 default['cookbook-openshift3']['openshift_node_sdn_mtu_sdn'] = '1450'
 # Deprecated options (Use openshift_node_kubelet_args_custom instead)
 default['cookbook-openshift3']['openshift_node_max_pod'] = ''
