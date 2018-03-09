bd83a60506646c5168871bdcca66b54ef47ed10c
William Burton <william17.burton@gmail.com>
2018-03-09 22:39:36 +0000

    Fix ETCD

--- recipes/adhoc_migrate_etcd.rb
+++ recipes/adhoc_migrate_etcd.rb
@@ -27,20 +27,24 @@ Dir["#{Chef::Config[:file_cache_path]}/etcd_migration*"].each do |path|
   end
 end
 
-if is_first_etcd
+if is_etcd_server
   log 'Check if there is at least one v2 snapshot [Abort if not found]' do
     level :info
   end
 
-  return unless Dir["#{node['cookbook-openshift3']['etcd_data_dir']}/member/snap/*.snap"].any?
+  node.run_state['issues_detected'] = true unless Dir["#{node['cookbook-openshift3']['etcd_data_dir']}/member/snap/*.snap"].any?
 
   execute 'Check if there are any v3 data [Abort if at least one v3 key]' do
-    command "[[ $(ETCDCTL_API=3 /usr/bin/etcdctl --cert #{node['cookbook-openshift3']['etcd_peer_file']} --key #{node['cookbook-openshift3']['etcd_peer_key']} --cacert #{node['cookbook-openshift3']['etcd_ca_cert']} --endpoints https://`hostname`:2379 get '.' --from-key --keys-only -w simple | wc -l) -gt 1 ]] && exit 1 || true"
+    command "[[ $(ETCDCTL_API=3 /usr/bin/etcdctl --cert #{node['cookbook-openshift3']['etcd_peer_file']} --key #{node['cookbook-openshift3']['etcd_peer_key']} --cacert #{node['cookbook-openshift3']['etcd_ca_cert']} --endpoints https://`hostname`:2379 get '.' --from-key --keys-only -w simple | wc -l) -gt 1 ]] && touch #{Chef::Config[:file_cache_path]}/etcd_migration-fail || true"
   end
+
+  node.run_state['issues_detected'] = true if Dir["#{Chef::Config[:file_cache_path]}/etcd_migration-fail"]
 
   execute 'Check cluster health' do
     command "/usr/bin/etcdctl --cert-file #{node['cookbook-openshift3']['etcd_peer_file']} --key-file #{node['cookbook-openshift3']['etcd_peer_key']} --ca-file #{node['cookbook-openshift3']['etcd_ca_cert']} -C https://`hostname`:2379 cluster-health | grep -w 'cluster is healthy'"
   end
+
+  include_recipe 'cookbook-openshift3::validate'
 end
 
 if is_master_server
