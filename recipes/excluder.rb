8e35eadcc59e9a8f046d1dbb627bf24c62fcf355
William Burton <william17.burton@gmail.com>
2018-03-11 08:47:54 +0000

    Fix logic for pkg excluder
    Fix excluder

--- recipes/excluder.rb
+++ recipes/excluder.rb
@@ -4,10 +4,14 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-ose_major_version = node['cookbook-openshift3']['deploy_containerized'] == true ? node['cookbook-openshift3']['openshift_docker_image_version'].reverse.chop.reverse : node['cookbook-openshift3']['ose_version'].to_s.split('-')[0]
+ose_major_version = node['cookbook-openshift3']['deploy_containerized'] == true ? node['cookbook-openshift3']['openshift_docker_image_version'] : node['cookbook-openshift3']['ose_major_version']
 
 %w(excluder docker-excluder).each do |pkg|
-  yum_package "#{node['cookbook-openshift3']['openshift_service_type']}-#{pkg} = #{ose_major_version}" unless ose_major_version.split('.')[1].to_i < 5
+  yum_package "#{node['cookbook-openshift3']['openshift_service_type']}-#{pkg}" do
+    version node['cookbook-openshift3']['excluder_version'] unless node['cookbook-openshift3']['excluder_version'].nil?
+    not_if { ose_major_version.split('.')[1].to_i < 5 }
+  end
+
   execute "Enable #{node['cookbook-openshift3']['openshift_service_type']}-#{pkg}" do
     command "#{node['cookbook-openshift3']['openshift_service_type']}-#{pkg} disable"
     not_if { ose_major_version.split('.')[1].to_i < 5 }
