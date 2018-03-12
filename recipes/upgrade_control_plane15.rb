512d14a8bc98c25a5c800ab9c386abaffc027195
William Burton <william17.burton@gmail.com>
2018-03-12 00:40:20 +0000

    Update control_upgrade

--- recipes/upgrade_control_plane15.rb
+++ recipes/upgrade_control_plane15.rb
@@ -20,21 +20,21 @@ if ::File.file?(node['cookbook-openshift3']['control_upgrade_flag'])
   node.force_override['cookbook-openshift3']['ose_version'] = '1.5.1-1.el7'
   node.force_override['cookbook-openshift3']['openshift_docker_image_version'] = 'v1.5.1'
   node.force_override['yum']['main']['exclude'] = 'docker-1.13* etcd-3.2*'
-  
+
   hosted_upgrade_version = node['cookbook-openshift3']['deploy_containerized'] == true ? node['cookbook-openshift3']['openshift_docker_image_version'] : 'v' + node['cookbook-openshift3']['ose_version'].to_s.split('-')[0]
-  
+
   server_info = OpenShiftHelper::NodeHelper.new(node)
   is_etcd_server = server_info.on_etcd_server?
   is_master_server = server_info.on_master_server?
   is_node_server = server_info.on_node_server?
   is_first_master = server_info.on_first_master?
-  
+
   if defined? node['cookbook-openshift3']['upgrade_repos']
     node.force_override['cookbook-openshift3']['yum_repositories'] = node['cookbook-openshift3']['upgrade_repos']
   end
-  
+
   include_recipe 'yum::default'
-  
+
   if is_master_server || is_node_server
     %w(excluder docker-excluder).each do |pkg|
       execute "Disable #{node['cookbook-openshift3']['openshift_service_type']}-#{pkg} (Best effort < 3.5)" do
@@ -43,60 +43,60 @@ if ::File.file?(node['cookbook-openshift3']['control_upgrade_flag'])
       end
     end
   end
-  
+
   if is_etcd_server
     log 'Upgrade for ETCD [STARTED]' do
       level :info
     end
-  
+
     execute 'Generate etcd backup before upgrade' do
       command "etcdctl backup --data-dir=#{node['cookbook-openshift3']['etcd_data_dir']} --backup-dir=#{node['cookbook-openshift3']['etcd_data_dir']}-pre-upgrade15"
       not_if { ::File.directory?("#{node['cookbook-openshift3']['etcd_data_dir']}-pre-upgrade15") }
       notifies :run, 'execute[Copy etcd v3 data store (PRE)]', :immediately
     end
-  
+
     execute 'Copy etcd v3 data store (PRE)' do
       command "cp -a #{node['cookbook-openshift3']['etcd_data_dir']}/member/snap/db #{node['cookbook-openshift3']['etcd_data_dir']}-pre-upgrade15/member/snap/"
       only_if { ::File.file?("#{node['cookbook-openshift3']['etcd_data_dir']}/member/snap/db") }
       action :nothing
     end
-  
+
     include_recipe 'cookbook-openshift3'
     include_recipe 'cookbook-openshift3::common'
     include_recipe 'cookbook-openshift3::etcd_cluster'
-  
+
     execute 'Generate etcd backup after upgrade' do
       command "etcdctl backup --data-dir=#{node['cookbook-openshift3']['etcd_data_dir']} --backup-dir=#{node['cookbook-openshift3']['etcd_data_dir']}-post-upgrade15"
       not_if { ::File.directory?("#{node['cookbook-openshift3']['etcd_data_dir']}-post-upgrade15") }
       notifies :run, 'execute[Copy etcd v3 data store (POST)]', :immediately
     end
-  
+
     execute 'Copy etcd v3 data store (POST)' do
       command "cp -a #{node['cookbook-openshift3']['etcd_data_dir']}/member/snap/db #{node['cookbook-openshift3']['etcd_data_dir']}-post-upgrade15/member/snap/"
       only_if { ::File.file?("#{node['cookbook-openshift3']['etcd_data_dir']}/member/snap/db") }
       action :nothing
     end
-  
+
     log 'Upgrade for ETCD [COMPLETED]' do
       level :info
     end
   end
-  
+
   if is_master_server
     log 'Upgrade for MASTERS [STARTED]' do
       level :info
     end
-  
+
     include_recipe 'cookbook-openshift3::certificate_server' if node['cookbook-openshift3']['deploy_containerized']
-  
+
     if node['cookbook-openshift3']['openshift_HA']
       include_recipe 'cookbook-openshift3::master_cluster'
     else
       include_recipe 'cookbook-openshift3::master_standalone'
     end
-  
+
     include_recipe 'cookbook-openshift3::node' if is_node_server
-  
+
     log 'Restart Master & Node services' do
       level :info
       notifies :restart, "service[#{node['cookbook-openshift3']['openshift_service_type']}-master]", :immediately unless node['cookbook-openshift3']['openshift_HA']
@@ -106,29 +106,29 @@ if ::File.file?(node['cookbook-openshift3']['control_upgrade_flag'])
       notifies :restart, 'service[openvswitch]', :immediately
       not_if { node['cookbook-openshift3']['deploy_containerized'] }
     end
-  
+
     log 'Upgrade for MASTERS [COMPLETED]' do
       level :info
     end
   end
-  
+
   if is_master_server && is_first_master
     log 'Reconcile Cluster Roles & Cluster Role Bindings [STARTED]' do
       level :info
     end
-  
+
     execute 'Wait for API to be ready' do
       command "[[ $(curl --silent #{node['cookbook-openshift3']['openshift_master_api_url']}/healthz/ready --cacert #{node['cookbook-openshift3']['openshift_master_config_dir']}/ca.crt --cacert #{node['cookbook-openshift3']['openshift_master_config_dir']}/ca-bundle.crt) =~ \"ok\" ]]"
       retries 120
       retry_delay 1
     end
-  
+
     execute 'Reconcile Cluster Roles' do
       command "#{node['cookbook-openshift3']['openshift_common_admin_binary']} \
               --config=#{node['cookbook-openshift3']['openshift_master_config_dir']}/admin.kubeconfig \
               policy reconcile-cluster-roles --additive-only=true --confirm"
     end
-  
+
     execute 'Reconcile Cluster Role Bindings' do
       command "#{node['cookbook-openshift3']['openshift_common_admin_binary']} \
               --config=#{node['cookbook-openshift3']['openshift_master_config_dir']}/admin.kubeconfig \
@@ -139,27 +139,27 @@ if ::File.file?(node['cookbook-openshift3']['control_upgrade_flag'])
               --exclude-users=system:anonymous \
               --additive-only=true --confirm"
     end
-  
+
     execute 'Reconcile Jenkins Pipeline Role Bindings' do
       command "#{node['cookbook-openshift3']['openshift_common_admin_binary']} \
               --config=#{node['cookbook-openshift3']['openshift_master_config_dir']}/admin.kubeconfig \
               policy reconcile-cluster-role-bindings system:build-strategy-jenkinspipeline --confirm"
     end
-  
+
     execute 'Reconcile Security Context Constraints' do
       command "#{node['cookbook-openshift3']['openshift_common_admin_binary']} \
               --config=#{node['cookbook-openshift3']['openshift_master_config_dir']}/admin.kubeconfig \
               policy reconcile-sccs --confirm --additive-only=true"
     end
-  
+
     log 'Reconcile Cluster Roles & Cluster Role Bindings [COMPLETED]' do
       level :info
     end
-  
+
     log 'Update hosted deployment(s) to current version [STARTED]' do
       level :info
     end
-  
+
     ruby_block 'Get current router image' do
       block do
         node.run_state['router_image'] = Mixlib::ShellOut.new("#{node['cookbook-openshift3']['openshift_common_client_binary']} get dc/router -n #{node['cookbook-openshift3']['openshift_hosted_router_namespace']} -o jsonpath='{.spec.template.spec.containers[0].image}'").run_command.stdout.strip
@@ -168,7 +168,7 @@ if ::File.file?(node['cookbook-openshift3']['control_upgrade_flag'])
         node['cookbook-openshift3']['openshift_hosted_manage_router']
       end
     end
-  
+
     ruby_block 'Get current registry image' do
       block do
         node.run_state['registry_image'] = Mixlib::ShellOut.new("#{node['cookbook-openshift3']['openshift_common_client_binary']} get dc/docker-registry -n #{node['cookbook-openshift3']['openshift_hosted_registry_namespace']} -o jsonpath='{.spec.template.spec.containers[0].image}'").run_command.stdout.strip
@@ -177,7 +177,7 @@ if ::File.file?(node['cookbook-openshift3']['control_upgrade_flag'])
         node['cookbook-openshift3']['openshift_hosted_manage_registry']
       end
     end
-  
+
     execute "Update router image to current version \"#{hosted_upgrade_version}\"" do
       command lazy {
         "#{node['cookbook-openshift3']['openshift_common_client_binary']} \
@@ -190,7 +190,7 @@ if ::File.file?(node['cookbook-openshift3']['control_upgrade_flag'])
         node['cookbook-openshift3']['openshift_hosted_manage_router']
       end
     end
-  
+
     execute "Update registry image to current version \"#{hosted_upgrade_version}\"" do
       command lazy {
         "#{node['cookbook-openshift3']['openshift_common_client_binary']} \
@@ -203,18 +203,18 @@ if ::File.file?(node['cookbook-openshift3']['control_upgrade_flag'])
         node['cookbook-openshift3']['openshift_hosted_manage_registry']
       end
     end
-  
+
     execute 'Upgrade job storage' do
       command "#{node['cookbook-openshift3']['openshift_common_admin_binary']} \
               --config=#{node['cookbook-openshift3']['openshift_master_config_dir']}/admin.kubeconfig \
               migrate storage --include=jobs --confirm"
     end
-  
+
     log 'Update hosted deployment(s) to current version [COMPLETED]' do
       level :info
     end
   end
-  
+
   if is_master_server || is_node_server
     %w(excluder docker-excluder).each do |pkg|
       yum_package "#{node['cookbook-openshift3']['openshift_service_type']}-#{pkg} = #{node['cookbook-openshift3']['ose_version'].to_s.split('-')[0]}"
--- recipes/upgrade_node37.rb
+++ recipes/upgrade_node37.rb
@@ -19,42 +19,42 @@ if ::File.file?(node['cookbook-openshift3']['control_upgrade_flag'])
   node.force_override['cookbook-openshift3']['ose_major_version'] = '3.7'
   node.force_override['cookbook-openshift3']['ose_version'] = '3.7.0-1.0.7ed6862'
   node.force_override['cookbook-openshift3']['openshift_docker_image_version'] = 'v3.7.0'
-  
+
   server_info = OpenShiftHelper::NodeHelper.new(node)
   is_node_server = server_info.on_node_server?
-  
+
   if defined? node['cookbook-openshift3']['upgrade_repos']
     node.force_override['cookbook-openshift3']['yum_repositories'] = node['cookbook-openshift3']['upgrade_repos']
   end
-  
+
   include_recipe 'yum::default'
-  
+
   if is_node_server
     log 'Upgrade for NODE [STARTED]' do
       level :info
     end
-  
+
     %w(excluder docker-excluder).each do |pkg|
       execute "Disable #{node['cookbook-openshift3']['openshift_service_type']}-#{pkg}" do
         command "#{node['cookbook-openshift3']['openshift_service_type']}-#{pkg} enable"
       end
     end
-  
+
     include_recipe 'cookbook-openshift3'
     include_recipe 'cookbook-openshift3::common'
     include_recipe 'cookbook-openshift3::node'
-  
-    log 'Node services' do
-      level :info
-      notifies :restart, "service[#{node['cookbook-openshift3']['openshift_service_type']}-node]", :immediately
-      notifies :restart, 'service[openvswitch]', :immediately
-      not_if { node['cookbook-openshift3']['deploy_containerized'] }
-    end
-  
+
+    # log 'Node services' do
+    #  level :info
+    #  notifies :restart, "service[#{node['cookbook-openshift3']['openshift_service_type']}-node]", :immediately
+    #  notifies :restart, 'service[openvswitch]', :immediately
+    #  not_if { node['cookbook-openshift3']['deploy_containerized'] }
+    # end
+
     log 'Upgrade for NODE [COMPLETED]' do
       level :info
     end
-  
+
     %w(excluder docker-excluder).each do |pkg|
       yum_package "#{node['cookbook-openshift3']['openshift_service_type']}-#{pkg} = #{node['cookbook-openshift3']['ose_version'].to_s.split('-')[0]}"
       execute "Enable #{node['cookbook-openshift3']['openshift_service_type']}-#{pkg}" do
