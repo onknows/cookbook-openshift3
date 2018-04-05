ace8c0cf1ba2ff5d2718f3f1f27eb60f64839328
William Burton <william17.burton@gmail.com>
2018-04-05 01:50:08 +0100

    New version 2.0.7

--- providers/openshift_create_pv.rb
+++ providers/openshift_create_pv.rb
@@ -13,33 +13,52 @@ end
 
 action :create do
   converge_by 'Create PV' do
+    directory "#{Chef::Config['file_cache_path']}/pv"
+
+    template "#{Chef::Config[:file_cache_path]}/pv_template.yaml" do
+      source 'pv_template.yaml.erb'
+    end
+
+    template "#{Chef::Config[:file_cache_path]}/pvc_template.yaml" do
+      source 'pvc_template.yaml.erb'
+    end
+
     new_resource.persistent_storage.each do |pv|
-      execute "Create Persistent Storage : #{pv['name']}" do
-        cwd node['cookbook-openshift3']['openshift_master_config_dir']
-        command "eval echo \'\{\"apiVersion\":\"v1\",\"kind\":\"PersistentVolume\",\"metadata\":\{\"name\":\"${name_pv}\"\},\"spec\":\{\"capacity\":\{\"storage\":\"${capacity_pv}\"\},\"accessModes\":[\"${access_modes_pv}\"],\"nfs\":\{\"path\":\"${path_pv}\",\"server\":\"${server_pv}\"\},\"persistentVolumeReclaimPolicy\":\"${volume_policy}\"\}\}\' | #{node['cookbook-openshift3']['openshift_common_client_binary']} create -f - --config=admin.kubeconfig"
-        environment(
-          'name_pv' => "#{pv['name']}-volume",
-          'capacity_pv' => pv['capacity'],
-          'access_modes_pv' => pv['access_modes'],
-          'path_pv' => pv['path'],
-          'server_pv' => pv['server'],
-          'volume_policy' => pv.key?('policy') ? pv['policy'] : 'Retain'
-        )
-        not_if "[[ `#{node['cookbook-openshift3']['openshift_common_client_binary']} get pv/${name_pv} --no-headers --config=admin.kubeconfig | wc -l` -eq 1 ]]"
+      ruby_block "Setup Persistent Storage : #{pv['name']}" do
+        block do
+          pv_template = YAML.load_file("#{Chef::Config[:file_cache_path]}/pv_template.yaml")
+          pv_template['metadata']['name'] = "#{pv['name']}-volume"
+          pv_template['spec']['capacity']['storage'] = pv['capacity']
+          pv_template['spec']['accessModes'] = [pv['access_modes']]
+          pv_template['spec']['nfs']['path'] = pv['path']
+          pv_template['spec']['nfs']['server'] = pv['server']
+          pv_template['spec']['persistentVolumeReclaimPolicy'] = pv.key?('policy') ? pv['policy'] : 'Retain'
+
+          file "#{Chef::Config[:file_cache_path]}/pv/#{pv['name']}-volume.yaml" do
+            content pv_template.to_yaml
+          end
+        end
       end
 
       next unless pv.key?('claim')
-      execute "Create Persistent Claim: #{pv['name']}" do
-        cwd node['cookbook-openshift3']['openshift_master_config_dir']
-        command "eval echo \'\{\"apiVersion\":\"v1\",\"kind\":\"PersistentVolumeClaim\",\"metadata\":\{\"name\":\"${name_pvc}\"\},\"spec\":\{\"resources\":\{\"requests\":\{\"storage\":\"${capacity_pvc}\"\}\},\"accessModes\":[\"${access_modes_pvc}\"]\}\}\' | #{node['cookbook-openshift3']['openshift_common_client_binary']} create -f - --config=admin.kubeconfig -n ${namespace}"
-        environment(
-          'name_pvc' => "#{pv['name']}-claim",
-          'capacity_pvc' => pv['capacity'],
-          'access_modes_pvc' => pv['access_modes'],
-          'namespace' => pv['claim']['namespace']
-        )
-        not_if "[[ `#{node['cookbook-openshift3']['openshift_common_client_binary']} get pvc/${name_pvc} --no-headers --config=admin.kubeconfig -n ${namespace} | wc -l` -eq 1 ]]"
+      ruby_block "Create Persistent Claim: #{pv['name']}-claim" do
+        block do
+          pvc_template = YAML.load_file("#{Chef::Config[:file_cache_path]}/pvc_template.yaml")
+          pvc_template['metadata']['name'] = "#{pv['name']}-claim"
+          pvc_template['metadata']['namespace'] = pv['claim']['namespace']
+          pvc_template['spec']['accessModes'] = [pv['access_modes']]
+          pvc_template['spec']['resources']['requests']['storage'] = pv['capacity']
+          pvc_template['spec']['volumeName'] = "#{pv['name']}-volume"
+
+          file "#{Chef::Config[:file_cache_path]}/pv/#{pv['name']}-claim.yaml" do
+            content pvc_template.to_yaml
+          end
+        end
       end
+    end
+
+    execute 'Apply Persistent Storage' do
+      command "#{node['cookbook-openshift3']['openshift_common_client_binary']} apply -f #{Chef::Config[:file_cache_path]}/pv --recursive"
     end
   end
 end
